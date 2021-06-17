7fb547254fad457aa30fcae8444fa746742477ff
oleg <oleg.pav.m@gmail.com>
2021-06-17 19:06:41 +0300

    News.

--- src/game/root.scenario.ts
+++ src/game/root.scenario.ts
@@ -4,6 +4,7 @@ import { TelegramContext } from '../telegram/telegram.context';
 enum ERootButtons {
     OPTIONS = 'Настройки',
     QUESTS = 'Задания',
+    NEWS = 'Новости и объявления',
 }
 
 @TgController('root')
@@ -11,11 +12,16 @@ export class RootScenario {
     @TgStateHandler()
     async root(ctx: TelegramContext): Promise<void> {
         await ctx.setState('root->mainMenuSelect');
-        await ctx.send('Добро пожаловать в тестовую версию бота...' +
-            '\n\n' +
-            'В этой версии всего три задания - Знакомство наоборот, Учись учить и Рандомный бар.' +
-            '\nВ зависимости от настроек будут попадаться соответствующие задания.' +
-            '\n\nПишите отзывы и предложения - @oPavlov', ctx.buttonList(ERootButtons));
+        await ctx.send(
+            'Добро пожаловать в тестовую версию бота...' +
+                '\n\n' +
+                'В этой версии всего три задания - Знакомство наоборот, Учись учить и Рандомный бар.' +
+                '\nВ зависимости от настроек будут попадаться соответствующие задания.' +
+                '\n\nПишите отзывы и предложения - @oPavlov' +
+                '\n\nНу а все новости, объявления и прочее вы можете получать по этой ссылке:' +
+                '\nhttps://t.me/joinchat/Y055xr64tcViMTdi',
+            ctx.buttonList(ERootButtons),
+        );
     }
 
     @TgStateHandler()
@@ -29,10 +35,18 @@ export class RootScenario {
                 await ctx.redirect('quest->questList');
                 break;
 
+            case ERootButtons.NEWS:
+                await ctx.redirect('root->showNews');
+                break;
+
             default:
                 await ctx.send('Похоже такой настройки нет...');
         }
+    }
 
-        RootScenario['mainMenuSelect'];
+    @TgStateHandler()
+    async showNews(ctx: TelegramContext): Promise<void> {
+        await ctx.setState('root->mainMenuSelect');
+        await ctx.send('Все новости и объявления публикуются тут:\nhttps://t.me/joinchat/Y055xr64tcViMTdi');
     }
 }
